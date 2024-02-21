3f822323447456c216c4b11052591846dae390d1
Neha-Ujjwal <nehaujjwal1614@gmail.com>
2024-02-21 14:57:35 +0530

    creating Thread completed

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -1,20 +1,21 @@
 "use client";
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
+import * as z from "zod";
 import { ThreadValidation } from "@/lib/validations/thread";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { usePathname, useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { Button } from "../ui/button";
+import { createThread } from "@/lib/actions/thread.actions";
 
 // import { updateUser } from "@/lib/actions/user.actions";
-
 
 interface Props {
   user: {
@@ -29,24 +30,29 @@ interface Props {
   btnTitle: string;
 }
 
+const PostThread = ({ userId }: { userId: string }) => {
+  const router = useRouter();
+  const pathname = usePathname();
 
+  const form = useForm({
+    resolver: zodResolver(ThreadValidation),
+    defaultValues: {
+      thread: "",
+      accountId: userId,
+    },
+  });
 
-const PostThread = ({userId}:{userId:string}) => {
-    const router = useRouter();
-    const pathname = usePathname();
-  
-    const form = useForm({
-      resolver: zodResolver(ThreadValidation),
-      defaultValues: {
-        thread:"",
-        accountId:userId,
-      },
+  const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
+    // await createThead
+    await createThread({
+      text: values.thread,
+      author: userId,
+      communityId: null,
+      path: pathname,
     });
 
-
-    const onSubmit= async () => {
-        // await createThead
-    }
+    router.push("/");
+  };
 
   return (
     <Form {...form}>
@@ -54,30 +60,28 @@ const PostThread = ({userId}:{userId:string}) => {
         className="flex flex-col justify-start gap-10"
         onSubmit={form.handleSubmit(onSubmit)}
       >
-
         <FormField
           control={form.control}
-         name="thread"
+          name="thread"
           render={({ field }) => (
-            <FormItem className="flex flex-col gap-3 w-full">
+            <FormItem className="mt-10 flex flex-col gap-3 w-full">
               {/* w-full means full width of container not screen */}
               <FormLabel className="text-base-semibold text-light-2">
                 Name
               </FormLabel>
               <FormControl className=" mt-10 no-focus border border-dark-4 bg-dark-3 text-light-1">
-                <textarea
-                rows={15}
-                  {...field}
-                />
+                <textarea rows={15} {...field} />
               </FormControl>
-              <FormMessage/>
+              <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit"  className="bg-primary-500">Post Thread</Button>
-        </form>
-        </Form>
-  )
-}
+        <Button type="submit" className="bg-primary-500">
+          Post Thread
+        </Button>
+      </form>
+    </Form>
+  );
+};
 
-export default PostThread
+export default PostThread;
--- lib/models/thread.model.ts
+++ lib/models/thread.model.ts
@@ -1,29 +1,34 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 
 const threadSchema = new mongoose.Schema({
- text:{
-    type:String,
-    required:true
- },
- author:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:'User',
-    required:true
- },
- community:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:'Community',
- },
- createdAt:{
-    type:Date,
-    default:Date.now
- },
- parentId:{ // this is for the commnet that we have on a thread  so each comment thread will have the id of its parent
-    type:String
- }
+  text: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  community: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Community",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  parentId: {
+    // this is for the commnet that we have on a thread  so each comment thread will have the id of its parent
+    type: String,
+  },
+  children: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thread",
+    },
+  ],
 });
 
-const Thread=mongoose.models.Thread || mongoose.model("Thread",threadSchema);
+const Thread = mongoose.models.Thread || mongoose.model("Thread", threadSchema);
 export default Thread;
-
-
