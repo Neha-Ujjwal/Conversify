06fb02da752b008c762e501917fa543ce254d36d
Anshbir-Singh <anshbir181@gmail.com>
2024-02-22 18:50:44 +0530

    started multi-level comment system

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -1,70 +1,91 @@
 "use server";
+
 import { revalidatePath } from "next/cache";
-import Thread from "../models/thread.model";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
+import Thread from "../models/thread.model";
 
 interface Params {
-  text: string;
-  author: string;
-  communityId: string | null;
+  userId: string;
+  username: string;
+  name: string;
+  bio: string;
+  image: string;
   path: string;
 }
 
-export async function createThread({
-  text,
-  author,
-  communityId,
+export async function updateUser({
+  userId,
+  username,
+  name,
+  bio,
+  image,
   path,
-}: Params) {
+}: Params): Promise<void> {
+  connectToDB();
+
   try {
-    connectToDB();
+    await User.findOneAndUpdate(
+      { id: userId },
+      { username: username.toLowerCase(), name, bio, image, onboarded: true },
+      { upsert: true }
+    );
 
-    const createThread = await Thread.create({
-      text,
-      author,
-      community: null,
-    });
-
-    //update user model
-    await User.findByIdAndUpdate(author, {
-      $push: { threads: createThread._id },
-    });
-    revalidatePath(path);
+    if (path === "/profile/edit") {
+      revalidatePath(path);
+    }
   } catch (error: any) {
-    throw new Error(`Error creating thread:${error.message}`);
+    throw new Error(`Failed to create/update user: ${error.message}`);
   }
 }
 
-export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+export async function fetchUser(userId: String) {
+  try {
+    connectToDB();
+
+    return await User.findOne({ id: userId });
+    // .populate({
+    //   path:'communities',
+    //   model : Community
+    // })
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user : ${error.message}`);
+  }
+}
+
+export async function fetchThreadById(id: string) {
   connectToDB();
-
-  //calculate the number of posts to skip
-  const skipAmount = (pageNumber - 1) * pageSize;
-
-  //Fetch the posts that have no parents (top-level threads....)
-
-  //beacuse that threads which dont have parents Their parentId is null or undefined
-  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
-    .sort({ createdAt: "desc" })
-    .skip(skipAmount)
-    .limit(pageSize)
-    .populate({ path: "author", model: User })
-    .populate({
-      path: "children",
-      populate: {
+  try {
+    //Todo adding community
+    const thread = await Thread.findById(id)
+      .populate({
         path: "author",
         model: User,
-        select: "_id name parentId image",
-      },
-    });
+        select: "_id id name image",
+      })
+      .populate({
+        path: "children",
+        populate: [
+          {
+            path: "author",
+            model: User,
+            select: "_id id name parentId image",
+          },
+          {
+            path: "children",
+            model: Thread,
+            populate: {
+              path: "author",
+              model: User,
+              select: "_id id name parentId image",
+            },
+          },
+        ],
+      })
+      .exec();
 
-  const totalPostsCount = await Thread.countDocuments({
-    parentId: { $in: [null, undefined] },
-  });
-
-  const posts = await postsQuery.exec();
-  const isNext = totalPostsCount > skipAmount + posts.length;
-
-  return { posts, isNext };
+    return thread;
+  } catch (error: any) {
+    throw new Error(`Error fetching threads:${error.message}`);
+  }
 }
