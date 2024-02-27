ad4a954ce1405e00750aacd8c58b0e27d161e162
Neha-Ujjwal <nehaujjwal1614@gmail.com>
2024-02-27 17:55:53 +0530

    Profile page addes

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
+import Thread from "../models/thread.model";
 
 interface Params {
   userId: string;
@@ -38,18 +39,41 @@ export async function updateUser({
   }
 }
 
-export async function fetchUser(userId:String){
-  try{
+export async function fetchUser(userId: String) {
+  try {
     connectToDB();
 
-    return await User
-    .findOne({id:userId})
+    return await User.findOne({ id: userId });
     // .populate({
     //   path:'communities',
     //   model : Community
     // })
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user : ${error.message}`);
   }
-  catch(error:any){
-    throw new Error (`Failed to fetch user : ${error.message}`);
+}
+
+export async function fetchUserPosts(userId: string) {
+  try {
+    connectToDB();
+    //find all threads authored by user with given user id
+
+    //TODO Populate community
+    const threads = await User.findOne({ id: userId }).populate({
+      path: "threads",
+      model: Thread,
+      populate: {
+        path: "children",
+        model: Thread,
+        populate: {
+          path: "author",
+          model: User,
+          select: "name image id",
+        },
+      },
+    });
+    return threads;
+  } catch (error: any) {
+    throw new Error(`failed to fetch user posts:${error.message}`);
   }
 }
