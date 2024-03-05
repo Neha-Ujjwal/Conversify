4124a25db882366bf428fd097416d3190c231027
Neha-Ujjwal <nehaujjwal1614@gmail.com>
2024-03-05 20:14:28 +0530

    activity page added

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import UserCard from "@/components/cards/UserCard";
 const page = async () => {
   const user = await currentUser();
 
@@ -17,16 +18,36 @@ const page = async () => {
 
   //fetch user
   const result = await fetchUsers({
-    userId:user.id,
-    searchString:"",
-    pageNumber:1,
-    pageSize:25
+    userId: user.id,
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 25,
   });
   return (
     <section>
-        <h1 className="head-text mb-10">search</h1>
-    </section>
-  )
-}
+      <h1 className="head-text mb-10">search</h1>
+      {/*search bar*/}
 
-export default page
+      <div className="mt-14 flex flex-col gap-9">
+        {result.users.length === 0 ? (
+          <p>No Users</p>
+        ) : (
+          <>
+            {result.users.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+                personType="User"
+              />
+            ))}
+          </>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default page;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -80,7 +80,6 @@ export async function fetchUserPosts(userId: string) {
   }
 }
 
-
 export async function fetchUsers({
   userId,
   searchString = "",
@@ -105,7 +104,7 @@ export async function fetchUsers({
 
     // Create an initial query object to filter users.
     const query: FilterQuery<typeof User> = {
-      id: { $ne: userId }, // Exclude the current user from the results.$ne means notequal to            
+      id: { $ne: userId }, // Exclude the current user from the results.$ne means notequal to
     };
 
     // If the search string is not empty, add the $or operator to match either username or name fields.
@@ -135,6 +134,35 @@ export async function fetchUsers({
     return { users, isNext };
   } catch (error) {
     console.error("Error fetching users:", error);
+    throw error;
+  }
+}
+
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
+
+    // Find all threads created by the user
+    const userThreads = await Thread.find({ author: userId });
+
+    // Collect all the child thread ids (replies) from the 'children' field of each user thread
+    const childThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children);
+    }, []);
+
+    // Find and return the child threads (replies) excluding the ones created by the same user
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId }, // Exclude threads authored by the same user
+    }).populate({
+      path: "author",
+      model: User,
+      select: "name image _id",
+    });
+
+    return replies;
+  } catch (error) {
+    console.error("Error fetching replies: ", error);
     throw error;
   }
 }
